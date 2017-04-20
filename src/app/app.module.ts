78b8da1caca79f737f811740da744b39a1467988
yesroh <ronaizacard@gmail.com>
2017-04-20 16:25:55 -0300

    mess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { LocalNotifications } from 'ionic-native';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,74 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  classDay: any;
+  notifications: any[] = [];
+  days: any[];
+  chosenHours: number;
+  chosenMinutes: number;
+
+  user = {
+    name: '',
+    localization: '',
+    classDay: '',
+    classHour: ''
+  }
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController, public geolocation: Geolocation) {
+    this.classDay = moment(new Date()).format()
+
+    this.chosenHours = new Date().getHours()
+    this.chosenMinutes = new Date().getMinutes()
+
+    this.days = [
+        {title: 'Segunda', dayCode: 1, checked: false},
+        {title: 'Terça', dayCode: 2, checked: false},
+        {title: 'Quarta', dayCode: 3, checked: false},
+        {title: 'Quinta', dayCode: 4, checked: false},
+        {title: 'Sexta', dayCode: 5, checked: false},
+        {title: 'Sábado', dayCode: 6, checked: false},
+        {title: 'Domingo', dayCode: 0, checked: false}
+    ]
+
+    this.loadMap();
 
   }
+
+
+
+   loadMap(){
+
+       this.geolocation.getCurrentPosition().then((position) => {
+
+         let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+         let mapOptions = {
+           center: latLng,
+           zoom: 15,
+           mapTypeId: this.google.maps.MapTypeId.ROADMAP
+         }
+
+         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+       }, (err) => {
+         console.log(err);
+       });
+
+     }
+
+   addNotifications(){
+     console.log()
+   }
+
+   save() {
+     console.log(this.user)
+   }
+
+   cancelAll(){
+
+   }
 
 }
